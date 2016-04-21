3d26a905c3d4a272e7d7487c95b54a963e473e43
manikantupadhyay <manikant.upadhyay@magicsw.com>
2016-04-21 09:27:34 +0530

    Changes for checkboxes

--- app/modules/home/viewCountCtrl.js
+++ app/modules/home/viewCountCtrl.js
@@ -4,6 +4,44 @@ angular.module("home", []).controller("ViewCountCtrl", function ($scope,$http,$w
   var responsejson;
   var graphMainArr=[];
   var graphInnerArr=[];
+  $scope.courses = [1,2,3,4,5];
+      $scope.selectedCourses = [];
+      $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+          list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+      };
+      $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+      };
+
+      $scope.users = [1,2,3,4,5];
+      $scope.selectedUsers = [];
+      $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+          list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+      };
+      $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+      };
+      $scope.myDate = new Date();
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+  $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
   $scope.name="To be prepared";
   $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   $http.defaults.headers.common['Content-Type'] = 'application/json';
