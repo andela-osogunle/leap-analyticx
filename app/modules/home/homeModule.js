ce9cb3919f9a5f1a9edc964c83d8d331aa89ae36
manikantupadhyay <manikant.upadhyay@magicsw.com>
2016-04-29 10:54:52 +0530

    Removed unwanted Comments

--- app/modules/home/homeModule.js
+++ app/modules/home/homeModule.js
@@ -1,13 +1,4 @@
 (function() {
   'use strict';
-
-  /**
-   * @ngdoc function
-   * @name app.module:homeModule
-   * @description
-   * # homeModule
-   * Module of the app
-   */
-
     angular.module('home', []);
 })();
